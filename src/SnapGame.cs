840b5eb34f4ae502dd54f55694483ceb4e8f4392
DesmondLee95 <Apple086zz@gmail.com>
2017-09-07 21:19:04 +0800

    Changed score text font.

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -51,9 +51,11 @@ namespace CardGames
 			Card top = myGame.TopCard;
 			if (top != null)
 			{
-				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.White, 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.White, 0, 40);
+				SwinGame.LoadFontNamed ("GameFont", "Chunkfive.otf", 24);
+
+				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.White, 0, 10);
+				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.White,"GameFont", 0, 105);
+				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.White,"GameFont", 0, 125);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
 			}
 			else
